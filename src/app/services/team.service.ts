cd770f5d61c6d3f872383cd2b5f53d05f0f9fe4d
Germán Arevalo <germanarevalojerez@gmail.com>
2020-03-29 08:49:27 -0500

    creation and implementation firebase endpoints

---
+++ src/app/services/team.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
+import { Team } from '../interfaces/team';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+export const TeamsTableHeaders = ['name', 'country', 'players'];
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeamService {
+  private teamDb: AngularFireList<Team>;
+
+  constructor(private db: AngularFireDatabase) { 
+    this.teamDb = this.db.list('/teams', ref => ref.orderByChild('name'));
+  }
+
+  getTeams(): Observable<Team[]> {
+    return this.teamDb.snapshotChanges().pipe(
+      map(changes => {
+        return changes.map(c => ({ $key: c.payload.key, ...c.payload.val() }));
+      })
+    );
+  }
+
+  addTeam(team: Team) {
+    return this.teamDb.push(team);
+  }
+
+  deleteTeam(id: string) {
+    this.db.list('/teams').remove(id);
+  }
+
+  editTeam(newTeamData) {
+    const $key = newTeamData.$key;
+    delete(newTeamData.$key);
+    this.db.list('/teams').update($key, newTeamData);
+  }
+}
